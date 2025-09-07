0e3eb2312cf24edb00e27d4a9a131f3755a59b2a
melike-kaya <59470323+melike-kaya@users.noreply.github.com>
2025-09-07 19:22:29 +0300

    Update outputs.tf

--- infra/lambda/outputs.tf
+++ infra/lambda/outputs.tf
@@ -1,1 +1,7 @@
 # Outputs
+output "function_name"  { value = aws_lambda_function.this.function_name }
+output "alias_name"     { value = aws_lambda_alias.live.name }
+output "apigw_url"      { value = aws_apigatewayv2_api.http.api_endpoint }
+output "cd_app"         { value = aws_codedeploy_app.lambda.name }
+output "cd_group"       { value = aws_codedeploy_deployment_group.lambda.deployment_group_name }
+output "alarm_errors"   { value = aws_cloudwatch_metric_alarm.lambda_errors.name }
