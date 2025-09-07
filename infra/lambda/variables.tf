5739064712bb6a5b3e678beb93fcb1f436de9004
melike-kaya <59470323+melike-kaya@users.noreply.github.com>
2025-09-07 19:21:40 +0300

    Update variables.tf

--- infra/lambda/variables.tf
+++ infra/lambda/variables.tf
@@ -1,1 +1,8 @@
 # Variables
+variable "name"         { type = string }           # örn: dockyard
+variable "env"          { type = string }           # dev | prod
+variable "region"       { type = string }
+variable "ecr_registry" { type = string }           # 123456789012.dkr.ecr.us-east-1.amazonaws.com
+variable "ecr_repo"     { type = string }           # demo-app-repo
+variable "memory_mb"    { type = number default = 256 }
+variable "timeout_s"    { type = number default = 10 }
